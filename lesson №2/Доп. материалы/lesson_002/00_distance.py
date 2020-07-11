150f2c802ac8640ccf069ef26c4640c1798796cf
TP <bunker-90@yandex.ru>
2020-07-11 15:55:56 +0400

    расчитал дистанцию между 3мя городами.

--- lesson №2/Доп. материалы/lesson_002/00_distance.py
+++ lesson №2/Доп. материалы/lesson_002/00_distance.py
@@ -12,15 +12,32 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {
-    'Moscow to London': ((((550 - 510) ** 2) + ((370 - 510) ** 2)) ** 0.5),
-    'London to Paris': ((((510 - 480) ** 2) + ((510 - 480) ** 2)) ** 0.5),
-    'Paris to Moscow': ((((480 - 550) ** 2) + ((480 - 370) ** 2)) ** 0.5),
-
-}
+Moscow = sites['Moscow']
+London = sites['London']
+Paris = sites['Paris']
 
 
-print(distances)
+moscow_london = (((Moscow[0] - London[0]) ** 2) + ((Moscow[1] - London[1]) ** 2)) ** 0.5
+london_paris = (((London[0] - Paris[0]) ** 2) + ((London[1] - Paris[1]) ** 2)) ** 0.5
+paris_moscow = (((Paris[0] - Moscow[0]) ** 2) + ((London[1] - Paris[1]) ** 2)) ** 0.5
+
+distances = {}
+
+distances[Moscow] = {}
+distances[Moscow][London] = moscow_london
+distances[Moscow][Paris] = paris_moscow
+distances[London] = {}
+distances[London][Paris] = london_paris
+distances[London][Moscow] = moscow_london
+distances[Paris] = {}
+distances[Paris][Moscow] = paris_moscow
+distances[Paris][London] = london_paris
+
+
+
+print(distances[Moscow][London])
+print(distances[London][Paris])
+print(distances[Paris][Moscow])
 
 
 
